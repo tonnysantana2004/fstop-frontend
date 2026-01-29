fc0a6756aa28b65533e67d195561cb5131748d9e
Tonny Santana <tonnysantanasantos@gmail.com>
2026-01-28 23:59:28 -0300

    refactor: preloader on login with less time

--- src/app/shared/guard/preloader-guard.ts
+++ src/app/shared/guard/preloader-guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn} from '@angular/router';
 
 export const preloaderGuard: CanActivateFn = async (route, state) => {
     await new Promise(resolve => {
-        setTimeout(resolve, 3000)
+        setTimeout(resolve, 1800)
     })
     return true;
 };
